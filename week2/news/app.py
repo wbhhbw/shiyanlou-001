663d255326f22ca30f94bd179bc7978e20e0a8e3
381174609 <381174609@qq.com>
2017-11-24 15:06:51 +0800

    modify app.py and index.html

--- week2/news/app.py
+++ week2/news/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os, json
 
 app = Flask(__name__)
 
@@ -8,7 +9,14 @@ def not_fount(error):
 
 @app.route('/')
 def index():
-    pass
+    json_list=[]
+    files_list = os.listdir('../files')
+    for filename in files_list:
+        with open(os.path.join('../files', filename), 'r') as f:
+            article_dict = json.loads(f.read())
+            json_list.append(article_dict)
+    return render_template('index.html', json_list=json_list)            
+
 
 @app.route('/files/<filename>')
 def file(filename):
