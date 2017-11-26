c68ff0fe867eb4db3d2d3a024a9c47382349f338
381174609 <381174609@qq.com>
2017-11-26 15:41:38 +0800

    use mysql modify app.py

--- week2/news/app.py
+++ week2/news/app.py
@@ -1,7 +1,38 @@
 from flask import Flask, render_template, redirect, url_for
 import os, json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/web_app'
+db = SQLAlchemy(app)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %s>' % self.name
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey(category.id))
+    content = db.Column(db.Text)
+    category = db.relationship('Category',
+     backref=db.backref('file', lazy='dynamic'))
+
+
+
+    def __init__(self, title, created_time, category_id, content):
+        
+
+
+        
 
 @app.errorhandler(404)
 def not_fount(error):
